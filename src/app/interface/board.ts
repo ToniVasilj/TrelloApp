d6624e7512f564d9850c827ef17d20461f2e35ef
ToniVasilj <tonivasilj94@gmail.com>
2024-08-10 19:23:53 +0200

    Remove more unnecessary code

--- src/app/interface/board.ts
+++ src/app/interface/board.ts
@@ -1,22 +1,12 @@
-export interface Card {
+export class Card {
   id: number;
   text: string;
 }
 
-export interface BList {
+export class BList {
   id: number;
   name: string;
   cards: Card[];
-}
-
-export interface iBoard {
-  id: number;
-  name: string;
-  blists: BList[];
-}
-
-export interface BoardData {
-  board: iBoard;
 }
 
 export class Board {
